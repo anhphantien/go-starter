9de9463b31603bbf13c923bbafa1600c1dcdf666
Anh Phan <phanlong315@gmail.com>
2022-06-29 01:26:04 +0700

    .

--- routers/auth.router.go
+++ routers/auth.router.go
@@ -3,16 +3,19 @@ package routers
 import (
 	"go-starter/handlers"
 	"go-starter/middlewares"
+	"net/http"
 
 	"github.com/gorilla/mux"
 )
 
 func AuthRouter(r *mux.Router) {
-	r.Handle("/auth/login", middlewares.
-		NewChain(
+	r.Handle(
+		"/auth/login",
+		middlewares.NewChain(
 			middlewares.A,
 			middlewares.B,
-		).
-		Then(handlers.AuthLogin),
-	)
+		).Then(
+			handlers.AuthLogin,
+		),
+	).Methods(http.MethodPost)
 }
