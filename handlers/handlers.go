1a79e113469e3951e165d1826caae6dc6fcc80cd
Anh Phan <phanlong315@gmail.com>
2022-07-04 17:02:46 +0700

    .

--- handlers/handlers.go
+++ handlers/handlers.go
@@ -3,6 +3,6 @@ package handlers
 import "go-starter/repositories"
 
 var (
-	userRepository = repositories.UserRepository{}
 	bookRepository = repositories.BookRepository{}
+	userRepository = repositories.UserRepository{}
 )
