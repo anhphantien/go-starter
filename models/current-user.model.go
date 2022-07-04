db8c46fd38770f5b738eedfc105e17d34050e29e
Anh Phan <phanlong315@gmail.com>
2022-07-04 16:55:39 +0700

    .

--- models/current-user.model.go
+++ models/current-user.model.go
@@ -1,12 +1,13 @@
 package models
 
-import "github.com/golang-jwt/jwt/v4"
-
 type CurrentUser struct {
 	ID        uint64 `json:"id"`
 	Username  string `json:"username"`
 	Role      string `json:"role"`
 	IssuedAt  int64  `json:"iat"`
 	ExpiresAt int64  `json:"exp"`
-	jwt.RegisteredClaims
+}
+
+func (CurrentUser) Valid() (err error) {
+	return err
 }
